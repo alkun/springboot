935c7ae60cfc7e57daa78a732e9aba76406c2b1a
kunaldhamma <kunal.dhamma@gmail.com>
2017-12-21 23:56:38 +0530

    Handling properties

--- springboot/src/main/java/com/alkun/springboot/java/Example.java
+++ springboot/src/main/java/com/alkun/springboot/java/Example.java
@@ -1,6 +1,10 @@
 package com.alkun.springboot.java;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class Example {
 
+	@Value("${rest.app.name}")
+	String appName;
     @RequestMapping("/")
     String home() {
-        return "Hello World!This is sample Rest Controller! Many more to come...";
+        return appName+ ":Hello World!This is sample Rest Controller! Many more to come...Detect change again";
     }
 
     public static void main(String[] args) throws Exception {
